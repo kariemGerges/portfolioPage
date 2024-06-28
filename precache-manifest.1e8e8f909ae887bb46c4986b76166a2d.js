daf02284c81c5d1e4c090af4e7fec00d1504ddf4
kgerges <kgerges@iu.edu>
2024-06-28 10:48:24 -0400

    Updates

---
+++ precache-manifest.1e8e8f909ae887bb46c4986b76166a2d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b893ec6f41c6c00e316bbe09e9f0a83b",
+    "url": "/portfolioPage/index.html"
+  },
+  {
+    "revision": "48c9e72527c07ee51614",
+    "url": "/portfolioPage/static/css/main.656737d9.chunk.css"
+  },
+  {
+    "revision": "8db72cdeddcfe8d85885",
+    "url": "/portfolioPage/static/js/2.4f76dc97.chunk.js"
+  },
+  {
+    "revision": "9c0c47d34de10fa07a7c",
+    "url": "/portfolioPage/static/js/3.cf3acbb1.chunk.js"
+  },
+  {
+    "revision": "48c9e72527c07ee51614",
+    "url": "/portfolioPage/static/js/main.0f4ddf7c.chunk.js"
+  },
+  {
+    "revision": "0aa214922665b6b48643",
+    "url": "/portfolioPage/static/js/runtime~main.a87fe0f2.js"
+  },
+  {
+    "revision": "74f126b85b9aba24fee8a971c7181949",
+    "url": "/portfolioPage/static/media/Guide Me.74f126b8.png"
+  },
+  {
+    "revision": "26b0781c257ac8924114aa29a5f107f7",
+    "url": "/portfolioPage/static/media/P1.26b0781c.jpg"
+  },
+  {
+    "revision": "77d4ea76a33451845d15e4c1c08e49c1",
+    "url": "/portfolioPage/static/media/StatesQuiz.77d4ea76.png"
+  },
+  {
+    "revision": "8055d8c6a9fd2ebb9b3fd959942df840",
+    "url": "/portfolioPage/static/media/StockMe.8055d8c6.png"
+  },
+  {
+    "revision": "d3852667e420c6dd92308b9959729eb0",
+    "url": "/portfolioPage/static/media/contact me phone2.d3852667.png"
+  },
+  {
+    "revision": "d1214ffecf0d4dde190cf7a1548d8720",
+    "url": "/portfolioPage/static/media/contact.d1214ffe.png"
+  },
+  {
+    "revision": "b1abe2f0f3d9fc990ad2d6ab4422f1bc",
+    "url": "/portfolioPage/static/media/donationPal.b1abe2f0.png"
+  },
+  {
+    "revision": "988c85bd937c9cd4db4489b5f47ca69a",
+    "url": "/portfolioPage/static/media/fulllogo.988c85bd.png"
+  },
+  {
+    "revision": "c474e55a76da5e625e085d5e3c7739ea",
+    "url": "/portfolioPage/static/media/portLight.c474e55a.jpg"
+  },
+  {
+    "revision": "91d7b975212e2450dc30ade723e7b1d7",
+    "url": "/portfolioPage/static/media/the code designer logo.91d7b975.png"
+  },
+  {
+    "revision": "0089105cab74502aaf7f0a129d1da8b2",
+    "url": "/portfolioPage/static/media/travia.0089105c.png"
+  }
+]);
